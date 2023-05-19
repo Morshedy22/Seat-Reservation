ed77a8238b2b169af7e8bd97f1ad175eec4022d8
Morshedy22 <o.ayman2149@nu.edu.eg>
2023-05-19 20:27:43 +0300

    Add Files

---
+++ Main.java
@@ -0,0 +1,140 @@
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.ArrayList;
+import java.util.Scanner;
+import javax.swing.*;
+
+public class Main extends JFrame
+{
+
+    // doul ana awl mshghl al program 3ayz ykoun mtkhzn fehom koul alm3lomat alfatt
+    // fna hakoun mkhznhom fee files awl malprogram yshtghl ana hgebhom mn alfile m3aya 3shan
+    // lw 3ayz a3ml ai haga 3lehom a3ml br7ty w arg3 tany al updates in file    
+
+    // static char[] seatsFirstClass = new char[25]; // from 1 to 20 // wmzwd mn 3ndy al memory bt3ty f br7ty
+    // static char[] seatsSecondClass = new char[45]; // from 1 to 40
+    // static char[] seatsThirdClass = new char[55]; // from 1 to 50
+
+    static ArrayList<User> allUsers = new ArrayList<>();
+    
+    // fana h3ml fucntion asmha init() de htkhzn alhgat al file alh7tgha fal variable
+    public static void init()
+    {
+        // User.resetTheatreReservations();  
+        try{
+            File file = new File("UserData.txt");
+            FileInputStream fis = new FileInputStream(file);
+            ObjectInputStream ois = new ObjectInputStream(fis);
+
+            allUsers = (ArrayList<User>)ois.readObject();
+            ois.close();
+            
+        }catch(FileNotFoundException e){
+            System.out.println(e);
+        }catch(IOException e){
+            System.out.println(e);
+        }catch(ClassNotFoundException e){
+            System.out.println(e);
+        }   
+    
+    }
+    
+    public static void release()
+    {
+        try{
+            File file = new File("UserData.txt");
+            FileOutputStream fos = new FileOutputStream(file);
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+
+            oos.writeObject(allUsers);
+            oos.flush();
+            oos.close();
+            
+        }catch(FileNotFoundException e){
+            System.out.println(e);
+        }catch(IOException e){
+            System.out.println(e);
+        }
+    }
+
+    public static boolean signUp()
+    {
+        String name;
+        String passcode;
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter your Name: ");
+        name = in.nextLine();
+        System.out.print("Enter your passcode: ");
+        passcode = in.nextLine();
+        
+        for(User user : allUsers)
+        {
+            if(user.getName() == name && user.getPasscode() == passcode)
+            {
+                System.out.println("(***User Exists***");
+                in.close();
+                return false;
+            }
+                
+        }
+        allUsers.add(new User(name, passcode));
+        System.out.println("User has been Added successfully!");
+        in.close();
+        return true;
+
+    }
+    
+    public static User login(String name, String passcode)
+    {
+        for(User user : allUsers)
+        {
+            if(user.getName().equals(name) && user.getPasscode().equals(passcode))
+                return user;
+        }
+        return null;
+    }    
+    
+    public static void main(String args[])
+    {
+        // funciton init-> initialize koul alna 3yzo wktbo foo2 al funciton foo2
+        init();
+        
+        // signUp();
+        
+        // for( User u : allUsers)
+        //     System.out.println(u);
+        
+        // String name, pass;
+        // Scanner in = new Scanner(System.in);
+        // // System.out.println();
+        // name = in.nextLine();
+        // pass = in.nextLine();
+        
+        // User curr = new User();
+        // try
+        // {
+        //     curr = login(name, pass);
+        // }catch(NullPointerException e)
+        // {
+        //     System.out.println(e);
+        // }
+
+        // curr.ticketReservation(4, 1);
+
+        // for(User u : allUsers)
+        // {
+        //     System.out.println(u);
+        //     u.reservedSeats();
+        // }
+        
+        // in.close();
+        release();
+    }
+}
---
+++ User.java
@@ -0,0 +1,196 @@
+import java.io.Serializable;
+import java.util.ArrayList;
+class User implements Serializable
+{
+    private String name;
+    private String passcode;
+    private int totalAmountPaid;
+    private ArrayList<Integer> reservedSeatsInFirstClass = new ArrayList<>();    
+    private ArrayList<Integer> reservedSeatsInSecondClass = new ArrayList<>();    
+    private ArrayList<Integer> reservedSeatsInThirdClass = new ArrayList<>();    
+    
+    static char[] seatsFirstClass = new char[25]; // from 1 to 20 // wmzwd mn 3ndy al memory bt3ty f br7ty
+    static char[] seatsSecondClass = new char[45]; // from 1 to 40
+    static char[] seatsThirdClass = new char[55]; // from 1 to 50
+    
+    public User() {
+    }
+    public User(String name, String passcode) {
+        this.name = name;
+        this.passcode = passcode;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    } 
+    public String getName() {
+        return name;
+    }
+
+    public void setPasscode(String passcode) {
+        this.passcode = passcode;
+    } 
+    public String getPasscode() {
+        return passcode;
+    }
+
+    public int getTotalAmountPaid() {
+        return totalAmountPaid;
+    }
+   
+    
+    public void reservedSeats()
+    {
+        System.out.print("Class 1: ");
+        for(Integer i : reservedSeatsInFirstClass)
+            System.out.print(i+" ");
+        
+        System.out.println();
+        
+        System.out.print("Class 2: ");
+        for(Integer i : reservedSeatsInSecondClass)
+        System.out.print(i+" ");
+        
+        System.out.println();
+        
+        System.out.print("Class 3: ");
+        for(Integer i : reservedSeatsInThirdClass)
+        System.out.print(i+" ");
+        
+        System.out.println();
+    }
+    
+    public void ticketReservation(int idx, int Class)
+    {
+
+        if (Class == 1)
+        {
+            //Ticket in First Class
+            if(seatsFirstClass[idx] == 'O')
+            {
+                seatsFirstClass[idx] = 'X';
+                totalAmountPaid += 50;   
+                reservedSeatsInFirstClass.add(idx);
+            }
+            else
+                System.out.println("This seat is already taken");
+        }
+
+
+        else if (Class == 2)
+        {
+          //Tickets in Second Class
+            if(seatsSecondClass[idx] == 'O')
+            {
+                seatsSecondClass[idx] = 'X';
+                totalAmountPaid += 15;   
+                reservedSeatsInSecondClass.add(idx);
+            }
+            else
+                System.out.println("This seat is already taken");   
+        }
+        else if (Class == 3)
+        {
+            //Tickets in Third Class
+            if(seatsThirdClass[idx] == 'O')
+            {
+                seatsThirdClass[idx] = 'X';
+                totalAmountPaid += 10;  
+                reservedSeatsInThirdClass.add(idx);
+            }
+            else
+                System.out.println("This seat is already taken");   
+        }
+
+    }
+    public void ticketCancellation(int idx, int Class)
+    {
+        if (Class == 1)
+        {
+            int flag = 0;
+            int indexToBeRemoved = 0;
+            for(int i = 0; i < reservedSeatsInFirstClass.size(); i++)
+            {
+                if (idx == reservedSeatsInFirstClass.get(i))
+                    {
+                        flag = 1;
+                        indexToBeRemoved = i;
+                    }
+            }
+            if(flag == 1)
+            {
+                // seatsFirstClass[idx] = 'O';
+                reservedSeatsInFirstClass.remove(indexToBeRemoved);
+                totalAmountPaid-=50;
+            }
+            else
+            {
+                System.out.println("This Seat is not yours");
+            }
+        }
+        else if (Class == 2)
+        {
+            int flag = 0;
+            int indexToBeRemoved = 0;
+            for(int i = 0; i < reservedSeatsInSecondClass.size(); i++)
+            {
+                if (idx == reservedSeatsInSecondClass.get(i))
+                    {
+                        flag = 1;
+                        indexToBeRemoved = i;
+                    }
+            }
+            if(flag == 1)
+            {
+                // seatsSecondClass[idx] = 'O';
+                reservedSeatsInSecondClass.remove(indexToBeRemoved);
+                totalAmountPaid-=15;
+            }
+            else
+            {
+                System.out.println("This Seat is not yours");
+            }
+        }
+        else if (Class == 3)
+        {
+            int flag = 0;
+            int indexToBeRemoved = 0;
+            for(int i = 0; i < reservedSeatsInThirdClass.size(); i++)
+            {
+                if (idx == reservedSeatsInThirdClass.get(i))
+                    {
+                        flag = 1;
+                        indexToBeRemoved = i;
+                    }
+            }
+            if(flag == 1)
+            {
+            //   seatsThirdClass[idx] = 'O';
+              reservedSeatsInThirdClass.remove(indexToBeRemoved);
+              totalAmountPaid -= 10;
+            }
+            else
+            {
+                System.out.println("This Seat is not yours");
+            }
+
+        }
+    }
+
+    public static void resetTheatreReservations()
+    {
+        for(int i = 0; i < 25; i++)
+            seatsFirstClass[i] = 'O';
+        for(int i = 0; i < 45; i++)
+            seatsSecondClass[i] = 'O';
+        for(int i = 0; i < 50; i++)
+            seatsThirdClass[i] = 'O';
+    }
+    @Override
+    public String toString() {
+        return "User [name=" + name + ", totalAmountPaid=" + totalAmountPaid + "]";
+    }
+    
+    
+    
+}
